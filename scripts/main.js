6a5b5fe4562274450b1217fe49f396df86bdb9d9
Ken Hilton <speaker@skule.ca>
2024-11-26 04:54:53 -0500

    Move script into separate file

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+matchMedia('print').addListener(m => {
+	if (!m.matches) return;
+	const spans = Array.from(document.querySelectorAll('section.contents a + span'));
+	const h1s = Array.from(document.querySelectorAll('h1[id]'));
+	// get inch to px conversion
+	const elem = document.createElement('test');
+	elem.style.fontSize = '1in';
+	document.body.appendChild(elem);
+	const inch = +getComputedStyle(elem).fontSize.slice(0, -2);
+	document.body.removeChild(elem);
+	for (let i = 0; i < h1s.length; ++i) {
+		spans[i].innerText = Math.floor(h1s[i].getBoundingClientRect().top / (9 * inch)) + 1;
+	}
+});
